cf40b12823ea6a0b478fa805cf9f74fdb0f5b3a7
JLopez-W <102275514+JLopez-W@users.noreply.github.com>
2023-03-08 13:30:19 +0700

    Add each character code to a new array

--- script.rb
+++ script.rb
@@ -4,13 +4,15 @@ shift = 1
 p message_array = message.split("")
 
 def secret_message(message_array, shift)
+  coded_message = []
   message_array.bytes do |num|
     if num.between?(48, 57) || num.between?(65, 90) || num.between?(97, 122) 
-      puts (num + shift).chr
+      coded_message.push(num + shift)
     elsif
-      puts num.chr
+      coded_message.push(num)
     end 
   end
+  p coded_message
 end
 
 secret_message(message, shift)
