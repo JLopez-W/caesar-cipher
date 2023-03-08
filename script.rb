13e4e67eaf74e33e04e78131b49b4ede7762e401
JLopez-W <102275514+JLopez-W@users.noreply.github.com>
2023-03-08 23:42:06 +0700

    Changed everything because using cycle wasn't working

--- script.rb
+++ script.rb
@@ -1,16 +1,36 @@
 message = "Be sure to drink your ovaltine."
-code_shift = 5
-num_cycle = Array(48..57).cycle(1)
-caps_cycle = Array(65..90).cycle(1)
-lower_cycle = Array(97..122).cycle(1)
-
 p message_array = message.bytes
+code_shift = 3
+num_array= Array(48..57)
+caps_array = Array(65..90)
+lower_array = Array(97..122)
 
-shifted_values = message_array.map do |num| 
-  if num_cycle.include?(num) || caps_cycle.include?(num) || lower_cycle.include?(num)
-   p num = num + code_shift
-  else num = num
-  end
-end
+index_values = message_array.map do |num| 
+  if num_array.include?(num)
+    index = num_array.find_index(num) 
+    new_index = index + code_shift
+      if new_index > 9
+      new_index -= 10
+      end
+    new_num = num_array[new_index]
+    elsif caps_array.include?(num)
+        index = caps_array.find_index(num) 
+        new_index = index + code_shift
+          if new_index > 25
+        new_index -= 26
+          end
+       new_num = caps_array[new_index]
+    elsif lower_array.include?(num)
+        index = lower_array.find_index(num) 
+        new_index = index + code_shift
+          if new_index > 25
+          new_index -= 26
+          end
+        new_num = lower_array[new_index]
+    else num
+        end
+    end
 
-p shifted_values
+p index_values
+
+p secret_message = index_values.map { |index| index.chr }.join("")
