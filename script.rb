45c4a6d9bdb32cf4d8b095dc1094b65adfd3154d
JLopez-W <102275514+JLopez-W@users.noreply.github.com>
2023-03-09 14:14:43 +0700

    Test cycle method on each array

--- script.rb
+++ script.rb
@@ -1,6 +1,6 @@
-message = "0"
+message = "Aa0"
 message_array = message.bytes
-code_shift = 12
+code_shift = 1
 num_array = Array(48..57).cycle
 caps_array = Array(65..90).cycle
 lower_array = Array(97..122).cycle
@@ -8,10 +8,24 @@ lower_array = Array(97..122).cycle
 
 p index = num_array.find_index(48)
 p shift_number = index + code_shift + 1
-  new_index = (shift_number).times do |i|
-  i = num_array.next
-  p i
+  new_num = (shift_number).times do |value|
+  value = num_array.next
+  p value
 end
 
 
+p index = caps_array.find_index(65)
+p shift_number = index + code_shift + 1
+  new_num = (shift_number).times do |value|
+  value = caps_array.next
+  p value
+end
+
+
+p index = lower_array.find_index(97)
+p shift_number = index + code_shift + 1
+  new_num = (shift_number).times do |value|
+  value = lower_array.next
+  p value
+end
 
