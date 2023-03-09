c146f31ec3b0c7b48b7cd815440b9e6745e7b50b
JLopez-W <102275514+JLopez-W@users.noreply.github.com>
2023-03-10 00:50:43 +0700

    Fix method for getting new values for coded message
    Make separate methods for each type of character: uppercase, lowercase, and numbers
    
    Add arrays as placeholders for new values
    
    Add rewind method to array cycles so that the count starts at the beginning of the array each time

--- script.rb
+++ script.rb
@@ -1,31 +1,67 @@
-message = "Aa0"
+message = "0AAa0"
 message_array = message.bytes
-code_shift = 1
+code_shift = 3
 num_array = Array(48..57).cycle
 caps_array = Array(65..90).cycle
 lower_array = Array(97..122).cycle
 
+new_nums = []
+new_caps = []
+new_lower = []
 
-p index = num_array.find_index(48)
-p shift_number = index + code_shift + 1
-  new_num = (shift_number).times do |value|
-  value = num_array.next
-  p value
+p message_array
+
+get_new_nums = message_array.each do |item| 
+  if item.between?(48, 57)
+     index = num_array.find_index(item)
+     shift_number = index + code_shift + 1
+     make_new_num_arrary = (shift_number).times do |i|
+     i = num_array.next
+     new_nums.push(i)
+     end 
+    original_index = message_array.find_index(item)
+    message_array[original_index] = new_nums[-1]
+    new_nums.clear
+    num_array.rewind
+  end
+end
+
+get_new_caps = message_array.each do |item| 
+  if item.between?(65, 90)
+     index = caps_array.find_index(item)
+     shift_number = index + code_shift + 1
+     make_new_caps_arrary = (shift_number).times do |i|
+     i = caps_array.next
+     new_caps.push(i)
+     end 
+    original_index = message_array.find_index(item)
+    message_array[original_index] = new_caps[-1]
+    new_caps.clear
+    caps_array.rewind
+  end
+end
+
+get_new_lower = message_array.each do |item| 
+  if item.between?(97, 122)
+     index = lower_array.find_index(item)
+     shift_number = index + code_shift + 1
+     make_new_lower_arrary = (shift_number).times do |i|
+     i = lower_array.next
+     new_lower.push(i)
+     end 
+    original_index = message_array.find_index(item)
+    message_array[original_index] = new_lower[-1]
+    new_lower.clear
+    lower_array.rewind
+  end
 end
 
 
-p index = caps_array.find_index(65)
-p shift_number = index + code_shift + 1
-  new_num = (shift_number).times do |value|
-  value = caps_array.next
-  p value
-end
-
-
-p index = lower_array.find_index(97)
-p shift_number = index + code_shift + 1
-  new_num = (shift_number).times do |value|
-  value = lower_array.next
-  p value
-end
-
+get_new_nums
+get_new_caps
+get_new_lower
+ 
+ p new_nums
+ p new_caps
+ p message_array 
+  
