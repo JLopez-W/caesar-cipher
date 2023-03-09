ffadef460ba7b62c535e8634b0f9262db6fcb024
JLopez-W <102275514+JLopez-W@users.noreply.github.com>
2023-03-09 13:53:08 +0700

    Start again to get array cycles to work.
    This part correctly cycles through the num_array

--- script.rb
+++ script.rb
@@ -1,36 +1,17 @@
-message = "Be sure to drink your ovaltine."
-p message_array = message.bytes
-code_shift = 3
-num_array= Array(48..57)
-caps_array = Array(65..90)
-lower_array = Array(97..122)
+message = "0"
+message_array = message.bytes
+code_shift = 12
+num_array = Array(48..57).cycle
+caps_array = Array(65..90).cycle
+lower_array = Array(97..122).cycle
 
-index_values = message_array.map do |num| 
-  if num_array.include?(num)
-    index = num_array.find_index(num) 
-    new_index = index + code_shift
-      if new_index > 9
-      new_index -= 10
-      end
-    new_num = num_array[new_index]
-    elsif caps_array.include?(num)
-        index = caps_array.find_index(num) 
-        new_index = index + code_shift
-          if new_index > 25
-        new_index -= 26
-          end
-       new_num = caps_array[new_index]
-    elsif lower_array.include?(num)
-        index = lower_array.find_index(num) 
-        new_index = index + code_shift
-          if new_index > 25
-          new_index -= 26
-          end
-        new_num = lower_array[new_index]
-    else num
-        end
-    end
 
-p index_values
+p index = num_array.find_index(48)
+p shift_number = index + code_shift + 1
+  new_index = (shift_number).times do |i|
+  i = num_array.next
+  p i
+end
 
-p secret_message = index_values.map { |index| index.chr }.join("")
+
+
