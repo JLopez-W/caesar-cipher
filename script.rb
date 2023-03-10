ff8375e49eb1bc16c9457aa16592000ed4762a2f
JLopez-W <102275514+JLopez-W@users.noreply.github.com>
2023-03-10 21:01:08 +0700

    Fix bug that didn't change values of duplicate characters in message array
    Change from 'each' to 'map’
    
    Add new empty array for adding updated message character values
    
    Refactor method to push each new value into the new array instead of replacing values in the original message array

--- script.rb
+++ script.rb
@@ -1,62 +1,55 @@
-message = "Be sure to drink your ovaltine."
+message = "Be sure to drink your Ovaltine."
 message_array = message.bytes
-code_shift = 12
-num_array = Array(48..57).cycle
-caps_array = Array(65..90).cycle
+code_shift = 1
 lower_array = Array(97..122).cycle
+caps_array = Array(65..90).cycle
+num_array = Array(48..57).cycle
 
+new_message_values = []
 new_nums = []
 new_caps = []
 new_lower = []
 
-get_new_nums = message_array.each do |item| 
-  if item.between?(48, 57)
-     index = num_array.find_index(item)
-     shift_number = index + code_shift + 1
-     make_new_num_arrary = (shift_number).times do |i|
-     i = num_array.next
-     new_nums.push(i)
-     end 
-    original_index = message_array.find_index(item)
-    message_array[original_index] = new_nums[-1]
-    new_nums.clear
-    num_array.rewind
-  end
-end
-
-get_new_caps = message_array.each do |item| 
-  if item.between?(65, 90)
-     index = caps_array.find_index(item)
-     shift_number = index + code_shift + 1
-     make_new_caps_arrary = (shift_number).times do |i|
-     i = caps_array.next
-     new_caps.push(i)
-     end 
-    original_index = message_array.find_index(item)
-    message_array[original_index] = new_caps[-1]
-    new_caps.clear
-    caps_array.rewind
-  end
-end
-
-get_new_lower = message_array.each do |item| 
+get_new_values = message_array.map { |item|  
   if item.between?(97, 122)
-     index = lower_array.find_index(item)
-     shift_number = index + code_shift + 1
-     make_new_lower_arrary = (shift_number).times do |i|
-     i = lower_array.next
-     new_lower.push(i)
-     end 
-    original_index = message_array.find_index(item)
-    message_array[original_index] = new_lower[-1]
+    index = lower_array.find_index(item)
+    shift_number = index + code_shift + 1 
+      (shift_number).times do |i|
+        i = lower_array.next
+        new_lower.push(i) 
+        end
+    new_value = new_lower[-1]
+    new_message_values.push(new_value)
     new_lower.clear
     lower_array.rewind
+  elsif item.between?(65, 90)
+    index = caps_array.find_index(item)
+    shift_number = index + code_shift + 1 
+      (shift_number).times do |i|
+        i = caps_array.next
+        new_caps.push(i) 
+        end
+    new_value = new_caps[-1]
+    new_message_values.push(new_value)
+    new_caps.clear
+    caps_array.rewind
+  elsif item.between?(48, 57)
+    index = num_array.find_index(item)
+    shift_number = index + code_shift + 1 
+      (shift_number).times do |i|
+        i = num_array.next
+        new_nums.push(i) 
+        end
+    new_value = new_nums[-1]
+    new_message_values.push(new_value)
+    new_nums.clear
+    num_array.rewind
+        else 
+     new_value = item
+     new_message_values.push(new_value)
   end
-end
+  }
 
-get_new_nums
-get_new_caps
-get_new_lower
-
-secret_message = message_array.map { |index| index.chr }.join("")
-p "The secret message is: '#{secret_message}' "
+message_array.replace(new_message_values)
+secret_message = message_array.map { |value| value.chr }.join("")
+p "The secret message is ...  '#{secret_message}' "
