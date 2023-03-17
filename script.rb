30eea3067e0662844be0b89599d1522b206518cb
JLopez-W <102275514+JLopez-W@users.noreply.github.com>
2023-03-17 22:20:15 +0700

    Refactor to work on DRY

--- script.rb
+++ script.rb
@@ -8,45 +8,30 @@ num_array = Array(48..57).cycle
 new_message_values = []
 temp_array = []
 
-message_array.map { |item|
+message_array.map { |item|  
   if item.between?(97, 122)
-    index = lower_array.find_index(item)
-    shift_number = index + code_shift + 1
-    shift_number.times do |i|
-      i = lower_array.next
-      temp_array.push(i)
-    end
-    new_value = temp_array[-1]
-    new_message_values.push(new_value)
-    temp_array.clear
-    lower_array.rewind
+     array = lower_array
   elsif item.between?(65, 90)
-    index = caps_array.find_index(item)
-    shift_number = index + code_shift + 1
-    shift_number.times do |i|
-      i = caps_array.next
-      temp_array.push(i)
-    end
-    new_value = temp_array[-1]
-    new_message_values.push(new_value)
-    temp_array.clear
-    caps_array.rewind
+     array = caps_array
   elsif item.between?(48, 57)
-    index = num_array.find_index(item)
+     array = num_array  
+  end
+   
+  if (item.between?(97, 122) || item.between?(65, 90) || item.between?(48, 57))
+    index = array.find_index(item)
     shift_number = index + code_shift + 1
     shift_number.times do |i|
-      i = num_array.next
-      temp_array.push(i)
-    end
-    new_value = temp_array[-1]
-    new_message_values.push(new_value)
-    temp_array.clear
-    num_array.rewind
-  else
-    new_value = item
-    new_message_values.push(new_value)
-  end
-}
+      i = array.next
+      temp_array.push(i)  
+       end
+      new_value = temp_array[-1]
+      temp_array.clear
+      array.rewind
+   else
+     new_value = item
+   end
+     new_message_values.push(new_value)
+  }  
 
 message_array.replace(new_message_values)
 secret_message = message_array.map { |value| value.chr }.join('')
